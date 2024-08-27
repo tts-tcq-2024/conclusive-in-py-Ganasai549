ad80da2610faaed9fad6c147872772802e45c6ed
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-08-27 14:57:30 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -1,5 +1,5 @@
 import unittest
-from typewise_alert import typewise_alert
+from typewise_alert import typewise-alert
 
 class TypewiseTest(unittest.TestCase):
     def test_infers_breach_as_per_limits(self):
